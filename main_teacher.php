35dba036949dbf8de4de661f4c7467944d82bec0
nutthapon <nut33nutthapon@gmail.com>
2016-04-17 14:08:22 +0700

    creat main function teacher

---
+++ main_teacher.php
@@ -0,0 +1,69 @@
+<?php 
+session_start(); 
+include('popup.php');
+
+if ($_SESSION['type'] != 1) {
+	echo "<script language='javascript'>";
+	echo "location='index.php';";
+	echo "</script>";
+}
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="image/icon.ico">
+	<link rel="stylesheet" href="style.css">
+	<title>เว็บไซต์ระบบฐานข้อมูลวัสดุครุภัณฑ์โรงเรียนอรรถวิทย์</title>
+</head>
+<body id="main">
+	<div style='width:100%; height:25%;'>
+		<div style='width:1000px; heigth:100%; margin:auto; padding-top:1%;'>
+			<div style='width:15%; heigth:100%; float:left;'>
+				<img src='image/logo_ats.png' style="margin-top:20px;" width='90%'>
+			</div>
+			<div style='width:83%; heigth:100%; float:left; padding-left:2%;'>
+				<h1 style="margin-top:55px;">เว็บไซต์ระบบฐานข้อมูลวัสดุครุภัณฑ์โรงเรียนอรรถวิทย์
+				</h1>
+			</div>
+		</div>
+	</div>
+
+	<div style='width:100%; height:10%;'></div>
+
+	<div style='width:100%; height:15%;'>
+		<div style='width:900px; height:100%; margin:auto;'>
+			<div id='menus' style='margin-left:30%;'>
+				<a href="show_lent_teacher.php">
+					<div style="width:25%; float:left; padding-top:4%; padding-left:5%;">
+						<img src="image/search.png" width="60">
+					</div>
+					<div style="width:70%; float:left; padding-top:6%;">
+						<font id="inMenu">ค้นหาข้อมูลวัสดุและครุภัณฑ์</font>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div style='width:100%; height:15%;'>
+		<div style='width:900px; height:100%; margin:auto;'>
+			<div id='menus' style='margin-left:30%;'>
+				<a href="lent_mda_teacher.php">
+					<div style="width:25%; float:left; padding-top:4%; padding-left:5%;">
+						<img src="image/rar.png" width="60">
+					</div>
+					<div style="width:70%; float:left; padding-top:6%;">
+						<font id="inMenu">ยืม / คืนวัสดุ ครุภัณฑ์</font>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div style='width:100%; height:28%;'></div>
+
+	<div style="background-color:#323232; width:100%; height:5%; text-align: center; padding-top: 13px; margin-bottom:0px;">
+		<font color="#ffffff">&copy Copyright By Attawit School Credit By Nutthapon.B</font>
+	</div>
+</body>
+</html>
--- popup.php
+++ popup.php
@@ -253,5 +253,70 @@ $numberApprove = mysql_num_rows($queryApprove);
 		</form>
 	</div>
 
+	<div id="popupTeacherLent" class="overlay">
+		<form name="formLent" action="lent_mda_teacher_process.php" method="POST">
+			<div class="popup">
+				<div style="font-size:30px;">รายการที่ต้องการยืม</div>
+				<a class="close" href="#">&times;</a>
+				<div class="content">
+					<?php 
+					$arrayId = $_POST['check'];
+					$count = count($arrayId);
+					$i = 0;
+
+					while ($i < $count) {
+						$id_data_mda = $arrayId[$i];
+						$sql = "SELECT `id_mda`,`detail` FROM data_mda WHERE id = $id_data_mda";
+						$query = mysql_query($sql);
+						$data = mysql_fetch_array($query);
+						$id_mda = $data['id_mda'];
+						$detail = $data['detail'];
+
+					echo '<input type="text" style="width:50px;" name="id_data_mda[]" value='.$id_data_mda.' readonly><input type="text" style="width:150px;" name="id_mda[]" value='.$id_mda.' readonly><input type="text" style="width:200px;" name="detail[]" value='.$detail.' readonly>';
+						$i++;
+					}
+
+					 ?>
+					<div style="float:left; margin:auto; margin-top:30px;">
+						<input id="bt3" type="submit" name="submit" class="textbox" value="ยืนยัน การยืม">
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<div id="popupTeacherReturn" class="overlay">
+		<form name="form3" action="return_mda_teacher_process.php" method="POST">
+			<div class="popup">
+				<div style="font-size:30px;">รายการที่ต้องการคืน</div>
+				<a class="close" href="#">&times;</a>
+				<div class="content">
+					<?php 
+					$arrayId = $_POST['check'];
+					$count = count($arrayId);
+					$i = 0;
+
+					while ($i < $count) {
+						$id_data_lent = $arrayId[$i];
+						$sql = "SELECT `id_mda`,`name_mda`,`id_data_mda` FROM lent_return WHERE id = $id_data_lent";
+						$query = mysql_query($sql);
+						$data = mysql_fetch_array($query);
+						$id_data_mda = $data['id_data_mda'];
+						$id_mda = $data['id_mda'];
+						$detail = $data['name_mda'];
+
+					echo '<input type="text" style="width:50px;" name="id_data_lent[]" value='.$id_data_lent.' readonly><input type="text" style="width:150px;" name="id_mda[]" value='.$id_mda.' readonly><input type="text" style="width:200px;" name="detail[]" value='.$detail.' readonly><input type="hidden" name="id_data_mda[]" value='.$id_data_mda.'>';
+						$i++;
+					}
+
+					 ?>
+					<div style="float:left; margin:auto; margin-top:30px;">
+						<input id="bt3" type="submit" name="submit" class="textbox" value="ยืนยัน การคืน">
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+
 </body>
 </html>
